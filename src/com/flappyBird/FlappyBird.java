3a7c080399ab88847770d3cff1f02072a50b8344
Adrian <jaz.x.prime@gmail.com>
2020-08-13 08:44:29 -0400

    Base of app
    Implemented:
    - jframe
    - renderer
    - timer
    - action listener

--- src/com/flappyBird/FlappyBird.java
+++ src/com/flappyBird/FlappyBird.java
@@ -1,6 +1,54 @@
 package com.flappyBird;
 
-public class FlappyBird {
-	console.log("wassup")
+import javax.swing.JFrame;
+import javax.swing.Timer;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+
+
+
+public class FlappyBird implements ActionListener {
+	
+	
+	public final int WIDTH =  800, HEIGHT = 800;
+	
+	public Renderer renderer;
+	
+	
+	// Constructor creates jframe and renderer
+	public FlappyBird() {
+		
+		JFrame jframe = new JFrame();
+		
+		renderer = new Renderer();
+		
+		Timer timer = new Timer(20, this);
+		
+		jframe.add(renderer);
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jframe.setSize(WIDTH, HEIGHT);
+		jframe.setResizable(false);
+		jframe.setVisible(true);
+		
+		timer.start();
+
+		
+	}
+
+	
+	
+	public void repaint(Graphics g) {
+		
+	}
+
+
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+
+		renderer.repaint();
+		
+	}
 }
---
+++ src/com/flappyBird/apprunner.java
@@ -0,0 +1,18 @@
+package com.flappyBird;
+
+
+public class apprunner {
+	
+	public static FlappyBird flappyBird;
+	public static void main(String[] args) {
+		
+		System.out.print("Program started");
+		
+		
+		flappyBird = new FlappyBird();
+
+		
+	}
+	
+
+}
